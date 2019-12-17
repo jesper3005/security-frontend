5c321627bf92db8312bcf6507fb0d47262a68832
Jesper Christensen <Jesper300595@gmail.com>
2019-12-17 23:21:03 +0100

    updated index to call account store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex'
+import { account } from './account/account'
+import { alert } from './account/alert'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     //Insert imported modules
+    account,
+    alert,
   }
 })
