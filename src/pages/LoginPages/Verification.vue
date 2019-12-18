2d1bcb97a317afb93a1d425aeb84ac2cde9eaba1
unknown <jesper300595@gmail.com>
2019-12-18 13:14:21 +0100

    Created verification page

---
+++ src/pages/LoginPages/Verification.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'Verification'
+}
+</script>
+
+<template>
+    <div>
+        <h1>Please check your email and type in the code here</h1>
+        <input type=number />
+    </div>
+</template>
