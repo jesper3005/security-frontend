a01a2b2299742773b099750d9bcdd789ed1bd0c6
Michael Due Pedersen <mdp@Danskbilferie.local>
2019-12-19 14:59:43 +0100

    Updated

--- src/components/Nav/MainNavBar.vue
+++ src/components/Nav/MainNavBar.vue
@@ -31,19 +31,19 @@ export default {
 
       <!-- Centered content -->
       <li>
-        <router-link v-bind:to="'/'" :key="1">HJEM</router-link>
+        <router-link v-bind:to="'/'" :key="1">HOME</router-link>
       </li>
       <li>
-        <router-link v-bind:to="'/hvem-er-vi'" :key="2">HVEM ER VI</router-link>
+        <router-link v-bind:to="'/hvem-er-vi'" :key="2">WHO ARE WE</router-link>
       </li>
       <li>
-        <router-link v-bind:to="'/profile'" :key="3" v-if="isVerified && isLoggedIn">BRUGER PROFIL</router-link>
+        <router-link v-bind:to="'/profile'" :key="3" v-if="isVerified && isLoggedIn">PROFILE</router-link>
       </li>
       <li>
-        <router-link v-bind:to="'/login'" :key="4" v-if="!isLoggedIn | !isVerified">LOG IND</router-link>
+        <router-link v-bind:to="'/login'" :key="4" v-if="!isLoggedIn | !isVerified">LOGIN</router-link>
       </li>
       <li v-if="isLoggedIn && isVerified">
-        <button class="logout" v-on:click="logout">Log out</button>
+        <button class="logout" v-on:click="logout">LOG OUT</button>
       </li>
     </ul>
   </div>
